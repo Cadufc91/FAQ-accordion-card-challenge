bd3c095893627c779ebcc87470559cd991d2a648
Cadufc91 <fernandes.cadu@gmail.com>
2021-12-22 13:08:14 -0300

    Atualizando JS com funcionalidade toggle resolvida

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-* Toggle ainda nao funcinando
+* Toggle ainda nao funcinando - corrigido e funcionando 100%
 * Projeto iniciado pela metodologia Mobile-first, versao desktop nao completa.
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const questions = document.querySelectorAll('.question')
 
 questions.forEach((question) => question.addEventListener('click', () => {
-
+    
+    
     if(question.parentNode.classList.contains('active')) {
         question.parentNode.classList.toggle('active');
 }
@@ -9,4 +10,4 @@ questions.forEach((question) => question.addEventListener('click', () => {
         questions.forEach((question) => question.parentNode.classList.remove('active'));
         question.parentNode.classList.add('active');
     }
-))
+}))
